a4179bdb36cc3cba180c49fddcf854492764af5c
Krista Blachian <krista.blachian@scifusion360.com>
2019-02-21 12:45:24 -0500

    created model and connection

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,17 @@
 package com.theironyard;
 
+import com.theironyard.model.DataSource;
+
 public class Main {
 
     public static void main(String[] args) {
+        DataSource datasource = new DataSource();
+        if(!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        datasource.close();
 
 
     }
